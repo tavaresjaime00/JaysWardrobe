0fbfd23e5ad71efffefb42c8261c1db69470d6dc
Jaime Tavares <tavaresjaime00@gmail.com>
2023-05-29 00:54:30 -0400

    Initial Commit

---
+++ Jays Wardrobe/Jays_WardrobeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jays_WardrobeApp.swift
+//  Jays Wardrobe
+//
+//  Created by Jaime Tavares on 2023-05-29.
+//
+
+import SwiftUI
+
+@main
+struct Jays_WardrobeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
